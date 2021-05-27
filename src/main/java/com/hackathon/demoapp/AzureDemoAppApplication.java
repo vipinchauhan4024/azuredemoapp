832a28e5e62cb089bd28284100a09a37d9e5e3bf
Chauhan Vipin <vipin.chauhan@volvo.com>
2021-05-27 09:30:03 +0530

    first commit

---
+++ src/main/java/com/hackathon/demoapp/AzureDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.demoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/demoapp/AzureDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.demoapp;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
